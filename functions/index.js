0cce78365ef0c4334eeecc296858492b0df8d8e7
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2023-03-22 11:32:41 +0700

    latest

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,14 @@
-const functions = require("firebase-functions");
+functions = require("firebase-functions");
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
+// // // Create and deploy your first functions
+// // // https://firebase.google.com/docs/functions/get-started
+
 // exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
+//     functions.logger.info("Hello logs!", {
+//         structuredData: true
+//     });
+//     response.send("Hello from Firebase!");
 // });
+
+
+// Path: functions\package.json
